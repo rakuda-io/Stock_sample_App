7354ccc65bd9010579d81b49bc6c4bfffaeb35e9
Yuki Kubota <rakudalms@gmail.com>
2021-05-22 11:35:54 +0900

    カテゴリー全体のnewsを取得（scrape2.rb）

---
+++ scrape2.rb
@@ -0,0 +1,23 @@
+require 'nokogiri'
+
+html = File.open('pitnews.html', 'r') { |f| f.read }
+doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+
+pitnews = []
+doc.xpath('/html/body/main/section').each_with_index do |section, index|
+  next if index.zero?
+  contents = {category: nil, news: []}
+  contents[:category] = section.xpath('./h6').first.text
+
+  section.xpath('./div/div').each do |node|
+    title = node.xpath('./p/strong/a').first.text
+    url = node.xpath('./p/strong/a').first['href']
+
+    news = {title: title, url: url}
+    contents[:news] << news
+  end
+
+  pitnews << contents
+end
+
+pp pitnews
