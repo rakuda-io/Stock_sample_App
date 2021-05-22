5ceeb51178f9aca6ef9e460299c4f7cc0516dfd8
Yuki Kubota <rakudalms@gmail.com>
2021-05-22 11:21:40 +0900

    pitnewsのカテゴリーをひとつだけ取得（scrape.rb）

--- scrape.rb
+++ scrape.rb
@@ -13,6 +13,24 @@ def write_file(path, text)
   File.open(path, 'w') { |file| file.write(text) }
 end
 
-html = get_from('https://finviz.com/quote.ashx?t=ko')
+get_from('https://masayuki14.github.io/pit-news/')
 
-write_file('finviz.html', html)
+require 'nokogiri'
+
+html = File.open('pitnews.html', 'r') { |f| f.read }
+doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+# nodes = doc.xpath('//h6')
+section = doc.xpath('/html/body/main/section[2]').first
+# category = section.xpath('./h6').first.text
+
+contents = {category: nil, news: []}
+contents[:category] = section.xpath('./h6').first.text
+
+section.xpath('./div/div').each do |node|
+  title = node.xpath('./p/strong/a').first.text
+  url = node.xpath('./p/strong/a').first['href']
+  news = {title: title, url: url}
+  contents[:news] << news
+end
+
+pp contents
