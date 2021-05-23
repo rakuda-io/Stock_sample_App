7ce36b79a81af492a072729611947b43c23a4617
Yuki Kubota <rakudalms@gmail.com>
2021-05-24 07:29:52 +0900

    techpit_refactoring

---
+++ techpit_lesson/scrape3.rb
@@ -0,0 +1,30 @@
+require 'net/http'
+require 'nokogiri'
+require 'json'
+
+def get_from(url)
+  Net::HTTP.get(URI(url))
+end
+
+def write_file(path, text)
+  File.open(path, 'w') { |file| file.write(text) }
+end
+
+def scrape_news(news)
+  {
+    title: news.xpath('./p/strong/a').first.text,
+    url: news.xpath('./p/strong/a').first['href']
+  }
+end
+
+def scrape_section(section)
+  {
+    category: section.xpath('./h6').first.text,
+    news: section.xpath('./div/div').map { |node| scrape_news(node)}
+  }
+end
+
+html = File.read('pitnews.html')
+doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+pitnews = doc.xpath('/html/body/main/section').map { |section| scrape_section(section) }
+write_file('pitnews.json', {petnews: pitnews}.to_json)
