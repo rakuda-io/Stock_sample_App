9c842b1b9e888ce45fd59b80df3d2ad196a14d54
Yuki Kubota <rakudalms@gmail.com>
2021-05-24 06:39:03 +0900

    ティッカーごとに取得、URLも表示

--- Stock_app_test/main_stock_test.rb
+++ Stock_app_test/main_stock_test.rb
@@ -1,7 +1,12 @@
 require 'net/http'
 require 'nokogiri'
+require 'open-uri'
 
-html = File.open('coca-cola.html', 'r') { |f| f.read }
+ticker_symbol = gets
+
+url = "https://finviz.com/quote.ashx?t=#{ticker_symbol}"
+html = URI.open(url)
+
 doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
 
 ticker = doc.xpath('//*[@id="ticker"]').text
@@ -9,17 +14,4 @@ cp_name = doc.xpath('/html/body/div[4]/div/table[1]//b').text
 dividend = doc.xpath('//td[@class="snapshot-td2"]/b')[36].text
 dividend_per = doc.xpath('//td[@class="snapshot-td2"]/b')[42].text
 
-# /html/body/div[4]/div/table[1]/tbody/tr/td/table[1]/tbody/tr[2]/td/a/b
-# contents = {category: nil, news: []}
-# contents[:category] = section.xpath('./h6').first.text
-
-# section.xpath('./div/div').each do |node|
-#   title = node.xpath('./p/strong/a').first.text
-#   url = node.xpath('./p/strong/a').first['href']
-#   news = {title: title, url: url}
-#   contents[:news] << news
-# end
-
-# pp contents
-puts ticker, cp_name, dividend, dividend_per
-puts
+puts ticker, cp_name, dividend, dividend_per, url
